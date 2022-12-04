d13e9d1923995056d6ceaa0c8f9d7f457de27dc6
Brian Yu <briehnyu@gmail.com>
2022-12-04 16:31:53 -0500

    Worked on authentication

---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, 
+    uniqueness: true, 
+    length: { in: 3..30 }, 
+    format: { without: URI::MailTo::EMAIL_REGEXP, message:  "can't be an email" }
+  validates :email, 
+    uniqueness: true, 
+    length: { in: 3..255 }, 
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { in: 6..255 }, allow_nil: true
+  
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user&.authenticate(password)
+  end
+
+  def reset_session_token!
+    self.update!(session_token: generate_unique_session_token)
+    self.session_token
+  end
+
+  private
+
+  def generate_unique_session_token
+    loop do
+      token = SecureRandom.base64
+      break token unless User.exists?(session_token: token)
+    end
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ApplicationRecord.transaction do 
+    puts "Destroying tables..."
+    # Unnecessary if using `rails db:seed:replant`
+    User.destroy_all
+  
+    puts "Resetting primary keys..."
+    # For easy testing, so that after seeding, the first `User` has `id` of 1
+    ApplicationRecord.connection.reset_pk_sequence!('users')
+  
+    puts "Creating users..."
+    # Create one user with an easy to remember username, email, and password:
+    User.create!(
+      name: 'Tai Kamiya', 
+      email: 'tai@digi.io', 
+      password: 'password'
+    )
+  
+    puts "Done!"
+  end
